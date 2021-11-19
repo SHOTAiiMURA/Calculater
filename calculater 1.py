9eb0581fbc29a9fe7da8e22c6e5540c0724b4546
Shota Iimura <hn.j.shota027503@gmail.com>
2021-11-19 19:42:59 +0900

    first commit

---
+++ calculater 1.py
@@ -0,0 +1,332 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'calculater 1TNZVcR.ui'
+##
+## Created by: Qt User Interface Compiler version 6.1.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
+from PyQt6 import QtCore, QtGui, QtWidgets
+
+
+
+
+
+class Ui_Form(QtWidgets.QWidget):
+    def __init__(self, Form):
+        super().__init__()
+        Form.setObjectName("Form")
+        Form.resize(424, 537)
+        self.verticalLayout_2 = QVBoxLayout(Form)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.textEdit = QTextEdit(Form)
+        self.textEdit.setObjectName(u"textEdit")
+        self.textEdit.setMaximumSize(QSize(16777215, 100))
+        self.textEdit.setStyleSheet(u"\n"
+"font: 24pt \"Century Gothic\";\n"
+"")
+
+        self.verticalLayout.addWidget(self.textEdit)
+
+        self.verticalLayout_3 = QVBoxLayout()
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.all_clear_botton = QPushButton(Form)
+        self.all_clear_botton.setObjectName(u"all_clear_botton")
+        self.all_clear_botton.setMaximumSize(QSize(120, 120))
+        self.all_clear_botton.setStyleSheet(u"background-color: rgb(255, 85, 0);\n"
+"font: 20pt \"Segoe UI\";\n"
+"")
+
+        self.horizontalLayout.addWidget(self.all_clear_botton)
+
+        self.clear_botton = QPushButton(Form)
+        self.clear_botton.setObjectName(u"clear_botton")
+        self.clear_botton.setMaximumSize(QSize(120, 120))
+        self.clear_botton.setStyleSheet(u"font: 28pt \"Century Gothic\";\n"
+"background-color: rgb(236, 228, 204);")
+
+        self.horizontalLayout.addWidget(self.clear_botton)
+
+        self.delete_botton = QPushButton(Form)
+        self.delete_botton.setObjectName(u"delete_botton")
+        self.delete_botton.setMaximumSize(QSize(120, 120))
+        self.delete_botton.setStyleSheet(u"\n"
+"font: 700 16pt \"Century Gothic\";\n"
+"background-color: rgb(236, 228, 204);")
+
+        self.horizontalLayout.addWidget(self.delete_botton)
+
+        self.division_botton = QPushButton(Form)
+        self.division_botton.setObjectName(u"division_botton")
+        self.division_botton.setMaximumSize(QSize(120, 120))
+        self.division_botton.setStyleSheet(u"font: 28pt \"Century Gothic\";\n"
+"background-color: rgb(236, 228, 204);")
+
+        self.horizontalLayout.addWidget(self.division_botton)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout)
+
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.seven_botton = QPushButton(Form)
+        self.seven_botton.setObjectName(u"seven_botton")
+        self.seven_botton.setMaximumSize(QSize(120, 120))
+        self.seven_botton.setStyleSheet(u"font: 28pt \"Century Gothic\";\n"
+"background-color: rgb(236, 228, 204);")
+
+        self.horizontalLayout_3.addWidget(self.seven_botton)
+
+        self.eight_botton = QPushButton(Form)
+        self.eight_botton.setObjectName(u"eight_botton")
+        self.eight_botton.setMaximumSize(QSize(120, 120))
+        self.eight_botton.setStyleSheet(u"font: 28pt \"Century Gothic\";\n"
+"background-color: rgb(236, 228, 204);")
+
+        self.horizontalLayout_3.addWidget(self.eight_botton)
+
+        self.nine_botton = QPushButton(Form)
+        self.nine_botton.setObjectName(u"nine_botton")
+        self.nine_botton.setMaximumSize(QSize(120, 120))
+        self.nine_botton.setStyleSheet(u"font: 28pt \"Century Gothic\";\n"
+"background-color: rgb(236, 228, 204);")
+
+        self.horizontalLayout_3.addWidget(self.nine_botton)
+
+        self.time_botton = QPushButton(Form)
+        self.time_botton.setObjectName(u"time_botton")
+        self.time_botton.setMaximumSize(QSize(120, 120))
+        self.time_botton.setStyleSheet(u"font: 28pt \"Century Gothic\";\n"
+"background-color: rgb(236, 228, 204);")
+
+        self.horizontalLayout_3.addWidget(self.time_botton)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout_3)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.four_botton = QPushButton(Form)
+        self.four_botton.setObjectName(u"four_botton")
+        self.four_botton.setMaximumSize(QSize(120, 120))
+        self.four_botton.setStyleSheet(u"font: 28pt \"Century Gothic\";\n"
+"background-color: rgb(236, 228, 204);")
+
+        self.horizontalLayout_2.addWidget(self.four_botton)
+
+        self.five_botton = QPushButton(Form)
+        self.five_botton.setObjectName(u"five_botton")
+        self.five_botton.setMaximumSize(QSize(120, 120))
+        self.five_botton.setStyleSheet(u"font: 28pt \"Century Gothic\";\n"
+"background-color: rgb(236, 228, 204);")
+
+        self.horizontalLayout_2.addWidget(self.five_botton)
+
+        self.six_botton = QPushButton(Form)
+        self.six_botton.setObjectName(u"six_botton")
+        self.six_botton.setMaximumSize(QSize(120, 120))
+        self.six_botton.setStyleSheet(u"font: 28pt \"Century Gothic\";\n"
+"background-color: rgb(236, 228, 204);")
+
+        self.horizontalLayout_2.addWidget(self.six_botton)
+
+        self.subtract_botton = QPushButton(Form)
+        self.subtract_botton.setObjectName(u"subtract_botton")
+        self.subtract_botton.setMaximumSize(QSize(120, 120))
+        self.subtract_botton.setStyleSheet(u"font: 28pt \"Century Gothic\";\n"
+"background-color: rgb(236, 228, 204);")
+
+        self.horizontalLayout_2.addWidget(self.subtract_botton)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout_2)
+
+        self.horizontalLayout_4 = QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.one_botton = QPushButton(Form)
+        self.one_botton.setObjectName(u"one_botton")
+        self.one_botton.setMaximumSize(QSize(120, 120))
+        self.one_botton.setStyleSheet(u"font: 28pt \"Century Gothic\";\n"
+"background-color: rgb(236, 228, 204);")
+
+        self.horizontalLayout_4.addWidget(self.one_botton)
+
+        self.two_botton = QPushButton(Form)
+        self.two_botton.setObjectName(u"two_botton")
+        self.two_botton.setMaximumSize(QSize(120, 120))
+        self.two_botton.setStyleSheet(u"font: 28pt \"Century Gothic\";\n"
+"background-color: rgb(236, 228, 204);")
+
+        self.horizontalLayout_4.addWidget(self.two_botton)
+
+        self.three_botton = QPushButton(Form)
+        self.three_botton.setObjectName(u"three_botton")
+        self.three_botton.setMaximumSize(QSize(120, 120))
+        self.three_botton.setStyleSheet(u"font: 28pt \"Century Gothic\";\n"
+"background-color: rgb(236, 228, 204);")
+
+        self.horizontalLayout_4.addWidget(self.three_botton)
+
+        self.plus_botton = QPushButton(Form)
+        self.plus_botton.setObjectName(u"plus_botton")
+        self.plus_botton.setMaximumSize(QSize(120, 120))
+        self.plus_botton.setStyleSheet(u"font: 28pt \"Century Gothic\";\n"
+"background-color: rgb(236, 228, 204);")
+
+        self.horizontalLayout_4.addWidget(self.plus_botton)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout_4)
+
+        self.horizontalLayout_5 = QHBoxLayout()
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.empty_botton = QPushButton(Form)
+        self.empty_botton.setObjectName(u"empty_botton")
+        self.empty_botton.setMaximumSize(QSize(120, 120))
+        self.empty_botton.setStyleSheet(u"background-color: rgb(170, 170, 170);")
+
+        self.horizontalLayout_5.addWidget(self.empty_botton)
+
+        self.zero_botton = QPushButton(Form)
+        self.zero_botton.setObjectName(u"zero_botton")
+        self.zero_botton.setMaximumSize(QSize(120, 120))
+        self.zero_botton.setStyleSheet(u"font: 28pt \"Century Gothic\";\n"
+"background-color: rgb(236, 228, 204);")
+
+        self.horizontalLayout_5.addWidget(self.zero_botton)
+
+        self.empty_botton_2 = QPushButton(Form)
+        self.empty_botton_2.setObjectName(u"empty_botton_2")
+        self.empty_botton_2.setMaximumSize(QSize(120, 120))
+        self.empty_botton_2.setStyleSheet(u"background-color: rgb(170, 170, 170);")
+
+        self.horizontalLayout_5.addWidget(self.empty_botton_2)
+
+        self.equal_botton = QPushButton(Form)
+        self.equal_botton.setObjectName(u"equal_botton")
+        self.equal_botton.setMaximumSize(QSize(120, 120))
+        self.equal_botton.setStyleSheet(u"font: 28pt \"Century Gothic\";\n"
+"background-color: rgb(236, 228, 204);")
+
+        self.horizontalLayout_5.addWidget(self.equal_botton)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout_5)
+
+
+        self.verticalLayout.addLayout(self.verticalLayout_3)
+
+
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+        self.one_botton.clicked.connect(lambda: self.addNumDisplay(str(1)))
+
+        self.two_botton.clicked.connect(lambda: self.addNumDisplay(str(2)))
+        self.three_botton.clicked.connect(lambda: self.addNumDisplay(str(3)))
+        self.four_botton.clicked.connect(lambda: self.addNumDisplay(str(4)))
+        self.five_botton.clicked.connect(lambda: self.addNumDisplay(str(5)))
+        self.six_botton.clicked.connect(lambda: self.addNumDisplay(str(6)))
+        self.seven_botton.clicked.connect(lambda: self.addNumDisplay(str(7)))
+        self.eight_botton.clicked.connect(lambda: self.addNumDisplay(str(8)))
+        self.nine_botton.clicked.connect(lambda: self.addNumDisplay(str(9)))
+        self.zero_botton.clicked.connect(lambda: self.addNumDisplay(str(0)))
+        self.equal_botton.clicked.connect(self.calculate)
+        self.plus_botton.clicked.connect(lambda: self.addNumDisplay("+"))
+        self.subtract_botton.clicked.connect(lambda: self.addNumDisplay("-"))
+        self.time_botton.clicked.connect(lambda: self.addNumDisplay("*"))
+        self.division_botton.clicked.connect(lambda: self.addNumDisplay("/"))
+        self.delete_botton.clicked.connect(self.delete)
+        self.clear_botton.clicked.connect(lambda: self.addNumDisplay("C"))
+        self.all_clear_botton.clicked.connect(self.all_clear)
+
+
+
+
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.all_clear_botton.setText(QCoreApplication.translate("Form", u"AC", None))
+        self.clear_botton.setText(QCoreApplication.translate("Form", u"C", None))
+        self.delete_botton.setText(QCoreApplication.translate("Form", u"Delete", None))
+        self.division_botton.setText(QCoreApplication.translate("Form", u"/", None))
+        self.seven_botton.setText(QCoreApplication.translate("Form", u"7", None))
+        self.eight_botton.setText(QCoreApplication.translate("Form", u"8", None))
+        self.nine_botton.setText(QCoreApplication.translate("Form", u"9", None))
+        self.time_botton.setText(QCoreApplication.translate("Form", u"*", None))
+        self.four_botton.setText(QCoreApplication.translate("Form", u"4", None))
+        self.five_botton.setText(QCoreApplication.translate("Form", u"5", None))
+        self.six_botton.setText(QCoreApplication.translate("Form", u"6", None))
+        self.subtract_botton.setText(QCoreApplication.translate("Form", u"-", None))
+        self.one_botton.setText(QCoreApplication.translate("Form", u"1", None))
+        self.two_botton.setText(QCoreApplication.translate("Form", u"2", None))
+        self.three_botton.setText(QCoreApplication.translate("Form", u"3", None))
+        self.plus_botton.setText(QCoreApplication.translate("Form", u"+", None))
+        self.empty_botton.setText(QCoreApplication.translate("Form", u"Empty", None))
+        self.zero_botton.setText(QCoreApplication.translate("Form", u"0", None))
+        self.empty_botton_2.setText(QCoreApplication.translate("Form", u"Empty", None))
+        self.equal_botton.setText(QCoreApplication.translate("Form", u"=", None))
+
+    def button_clicked(self):
+        print("clicked")
+
+    def addNumDisplay(self, char):
+        display = self.textEdit.toPlainText()
+        print(display)
+        self.textEdit.setText(display+char)
+    def all_clear(self):
+        self.textEdit.setText("")
+
+    def delete(self):
+        display = self.textEdit.toPlainText()
+
+        if len(display) >= 1:
+            self.textEdit.setText(display[:-1])
+
+    def calculate(self):
+        display = self.textEdit.toPlainText()
+
+        try:
+            answer = eval(display)
+        except:
+            print("Error")
+            answer = "Error"
+
+        self.textEdit.setText(str(answer))
+
+
+    def add(self, a, b):
+        return a + b
+    def subtract(self, c, d):
+        return c - d
+    def multiply(self, e, f):
+        return e * f
+    def division(self, g, h):
+        return g / h
+
+
+
+
+    # retranslateUi
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form(Form)
+    Form.show()
+    sys.exit(app.exec())
